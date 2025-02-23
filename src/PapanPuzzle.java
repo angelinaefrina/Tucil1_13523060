e2820cbd9ca6a7d79baff38b65c592e9b9110309
angelinaefrina <efrinaprahastaputri@gmail.com>
2025-02-23 07:20:30 +0700

    ✨ feat: PapanPuzzle.java

---
+++ src/PapanPuzzle.java
@@ -0,0 +1,11 @@
+public class PapanPuzzle {
+    public char[][] papan;
+    public int indeks;
+    public int count;
+
+    public PapanPuzzle(char[][] papan, int indeks, int count) {
+        this.papan = papan;
+        this.indeks = indeks;
+        this.count = count;
+    }
+}
